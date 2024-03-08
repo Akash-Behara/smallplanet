b5044d1ab076f755296527f6b762cf45dc7e0354
AkashBehara <behara.akash68@gmail.com>
2024-03-08 13:03:47 +0530

    up

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ document.addEventListener('DOMContentLoaded', () => {
     );
     const N = 10;
     // [...Array(N).keys()]
-    const arcsData = ['Hi'].map(() => ({
-        startLat: (Math.random() - 0.5) * 180,
-        startLng: (Math.random() - 0.5) * 360,
-        endLat: (Math.random() - 0.5) * 180,
-        endLng: (Math.random() - 0.5) * 360,
-        color: [['red', 'black', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'black', 'blue', 'green'][Math.round(Math.random() * 3)]]
-    }));
+    // const arcsData = ['Hi'].map(() => ({
+    //     startLat: (Math.random() - 0.5) * 180,
+    //     startLng: (Math.random() - 0.5) * 360,
+    //     endLat: (Math.random() - 0.5) * 180,
+    //     endLng: (Math.random() - 0.5) * 360,
+    //     color: [['red', 'black', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'black', 'blue', 'green'][Math.round(Math.random() * 3)]]
+    // }));
 
-    let worldWidth = 0
-    let worldHeight = 0
+    // let worldWidth = 0
+    // let worldHeight = 0
 
-    if(window.innerWidth > 768){
-        worldWidth = 700
-        worldHeight = 700
-    }
-    if(window.innerWidth > 766 && window.innerWidth < 900){
-        worldWidth = 500
-        worldHeight = 500
-    } else {
-        worldWidth = 1000
-        worldHeight = 700
-    }
+    // if(window.innerWidth > 768){
+    //     worldWidth = 700
+    //     worldHeight = 700
+    // }
+    // if(window.innerWidth > 766 && window.innerWidth < 900){
+    //     worldWidth = 500
+    //     worldHeight = 500
+    // } else {
+    //     worldWidth = 1000
+    //     worldHeight = 700
+    // }
     
     // fetch('https://github.com/Akash-Behara/smallplanet/ne_110m_admin_0_countries.geojson', {
     //     headers: {
@@ -35,79 +35,79 @@ document.addEventListener('DOMContentLoaded', () => {
     //     }
     // }).then(res => res.json()).then(countries =>
     // {
-    const world = Globe()
-        .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-        // .hexPolygonsData(countries.features)
-        // .hexPolygonResolution(3)
-        // .hexPolygonMargin(0.7)
-        // .hexPolygonUseDots(true)
-        // .hexPolygonColor(() => "rgba(255,255,255, 1)")
-        .arcsData(arcsData)
-        .arcColor('color')
-        .arcDashLength(() => Math.random())
-        .arcDashGap(() => Math.random())
-        .arcDashAnimateTime(() => Math.random() * 2000 + 500)
-        .backgroundColor('rgba(0, 0, 0, 0)')
-        .pointOfView({ altitude: 5 })
-        (document.getElementById('globe'))
-        world.controls().autoRotate = true
-        world.controls().autoRotateSpeed = 0.9
-        world.controls().enableZoom = false;
-        world.width([worldWidth])
-        world.height([worldHeight])
-        window.addEventListener('resize', () => {
-            if(window.innerWidth > 768){
-                worldWidth = 700
-                worldHeight = 700
-                world.width([worldWidth])
-                world.height([worldHeight])
-            }
-            if(window.innerWidth > 766 && window.innerWidth < 900){
-                worldWidth = 700
-                worldHeight = 700
-                world.width([550])
-                world.height([550])
-            } else {
-                world.width([1000])
-                world.height([700])
-            }    
-        })
+    // const world = Globe()
+    //     .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+    //     // .hexPolygonsData(countries.features)
+    //     // .hexPolygonResolution(3)
+    //     // .hexPolygonMargin(0.7)
+    //     // .hexPolygonUseDots(true)
+    //     // .hexPolygonColor(() => "rgba(255,255,255, 1)")
+    //     .arcsData(arcsData)
+    //     .arcColor('color')
+    //     .arcDashLength(() => Math.random())
+    //     .arcDashGap(() => Math.random())
+    //     .arcDashAnimateTime(() => Math.random() * 2000 + 500)
+    //     .backgroundColor('rgba(0, 0, 0, 0)')
+    //     .pointOfView({ altitude: 5 })
+    //     (document.getElementById('globe'))
+    //     world.controls().autoRotate = true
+    //     world.controls().autoRotateSpeed = 0.9
+    //     world.controls().enableZoom = false;
+    //     world.width([worldWidth])
+    //     world.height([worldHeight])
+    //     window.addEventListener('resize', () => {
+    //         if(window.innerWidth > 768){
+    //             worldWidth = 700
+    //             worldHeight = 700
+    //             world.width([worldWidth])
+    //             world.height([worldHeight])
+    //         }
+    //         if(window.innerWidth > 766 && window.innerWidth < 900){
+    //             worldWidth = 700
+    //             worldHeight = 700
+    //             world.width([550])
+    //             world.height([550])
+    //         } else {
+    //             world.width([1000])
+    //             world.height([700])
+    //         }    
+    //     })
     // }).catch((err) => console.log('err', err));
 
     gsap.registerPlugin(ScrollTrigger);
 
     //SD SECTION
-    let tl_sd = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".SD",
-                toggleActions: "restart complete reverse resume",
-                start: "top top",
-                // markers: true,
-                scrub: true,
-                pin: true
-            }
-    })
-    tl_sd.to(".p-one", {
-        autoAlpha: 0
-    });
-    tl_sd.from(".p-two", {
-        autoAlpha: 0,
-        // y: 20
-    });
-    tl_sd.set(
-        ".img-one",
-        {
-        autoAlpha: 0
-        },
-        "<"
-    );
-    tl_sd.from(
-        ".img-two",
-        {
-        autoAlpha: 0
-        },
-        ">"
-    );
+    // let tl_sd = gsap.timeline({
+    //         scrollTrigger: {
+    //             trigger: ".SD",
+    //             toggleActions: "restart complete reverse resume",
+    //             start: "top top",
+    //             // markers: true,
+    //             scrub: true,
+    //             pin: true
+    //         }
+    // })
+    // tl_sd.to(".p-one", {
+    //     autoAlpha: 0
+    // });
+    // tl_sd.from(".p-two", {
+    //     autoAlpha: 0,
+    //     // y: 20
+    // });
+    // tl_sd.set(
+    //     ".img-one",
+    //     {
+    //     autoAlpha: 0
+    //     },
+    //     "<"
+    // );
+    // tl_sd.from(
+    //     ".img-two",
+    //     {
+    //     autoAlpha: 0
+    //     },
+    //     ">"
+    // );
     //SD SECTION END
 
     // //MOVE INFO IMG TO LEFT
@@ -141,15 +141,15 @@ document.addEventListener('DOMContentLoaded', () => {
    });
    // PROCESS END
 
-    window.addEventListener('scroll', () => {
-        console.log('scrol')
-        const g = document.getElementById('globe')
-        if(window.scrollY < 780){
-            // g.style.transform = `translateX(-${window.scrollY}px)`
-            window.innerWidth < 1005 ? g.style.transform = `translate(calc(-${window.scrollY}px + 100px), ${window.scrollY}px)` :
-            g.style.transform = `translate(-${window.scrollY}px, ${window.scrollY}px)`
-        }
-    })
+    // window.addEventListener('scroll', () => {
+    //     console.log('scrol')
+    //     const g = document.getElementById('globe')
+    //     if(window.scrollY < 780){
+    //         // g.style.transform = `translateX(-${window.scrollY}px)`
+    //         window.innerWidth < 1005 ? g.style.transform = `translate(calc(-${window.scrollY}px + 100px), ${window.scrollY}px)` :
+    //         g.style.transform = `translate(-${window.scrollY}px, ${window.scrollY}px)`
+    //     }
+    // })
 
 
     const cube1 = document.querySelector('.cube');
@@ -196,78 +196,75 @@ document.addEventListener('DOMContentLoaded', () => {
         //     }, 3500)
         // }
     }
-    checkVisibilityAndLog();
+    // checkVisibilityAndLog();
     // window.addEventListener('scroll', checkVisibilityAndLog);
 
 
     // CAROUSEL SECTION
-    let arr = [
-        {title: 'title1', description: 'description', image: 'image'},
-        {title: 'title2', description: 'description', image: 'image'},
-        {title: 'title3', description: 'description', image: 'image'},
-        {title: 'title4', description: 'description', image: 'image'},
-        {title: 'title5', description: 'description', image: 'image'},
-        {title: 'title5', description: 'description', image: 'image'},
-        {title: 'title5', description: 'description', image: 'image'},
-        {title: 'title5', description: 'description', image: 'image'},
-    ]
-    let carouselItems = [];
-    let carouselNavItems = [];
-    let currentSlide = '#slide-0';
-    const carouselItem = (item, i) => {
-        return `
-            <div id="slide-${i}" class="carousel__item">
-                <h1>${item.title}</h1>
-                <p>${item.description}</p>
-                <img src=${item.image} alt="">
-            </div>
-        `
-    }
-    const carouselNav = (i) => {
-        return `
-            <a href="#slide-${i}" id="${i}" class="slider-nav-item"></a>
-        `
-    }
+    // let arr = [
+    //     {title: 'title1', description: 'description', image: 'image'},
+    //     {title: 'title2', description: 'description', image: 'image'},
+    //     {title: 'title3', description: 'description', image: 'image'},
+    //     {title: 'title4', description: 'description', image: 'image'},
+    //     {title: 'title5', description: 'description', image: 'image'},
+    //     {title: 'title5', description: 'description', image: 'image'},
+    //     {title: 'title5', description: 'description', image: 'image'},
+    //     {title: 'title5', description: 'description', image: 'image'},
+    // ]
+    // let carouselItems = [];
+    // let carouselNavItems = [];
+    // let currentSlide = '#slide-0';
+    // const carouselItem = (item, i) => {
+    //     return `
+    //         <div id="slide-${i}" class="carousel__item">
+    //             <h1>${item.title}</h1>
+    //             <p>${item.description}</p>
+    //             <img src=${item.image} alt="">
+    //         </div>
+    //     `
+    // }
+    // const carouselNav = (i) => {
+    //     return `
+    //         <a href="#slide-${i}" id="${i}" class="slider-nav-item"></a>
+    //     `
+    // }
     
-    const carouselContainer = document.getElementById('Slider')
-    const caroudleNavContainer = document.getElementById('slider-nav')
-    for(let i = 0; i < arr.length; i++){
-        carouselItems.push(carouselItem(arr[i], i))
-        carouselNavItems.push(carouselNav(i))
-    }
-    carouselContainer.innerHTML = carouselItems.join('')
-    caroudleNavContainer.innerHTML = carouselNavItems.join('')
+    // const carouselContainer = document.getElementById('Slider')
+    // const caroudleNavContainer = document.getElementById('slider-nav')
+    // for(let i = 0; i < arr.length; i++){
+    //     carouselItems.push(carouselItem(arr[i], i))
+    //     carouselNavItems.push(carouselNav(i))
+    // }
+    // carouselContainer.innerHTML = carouselItems.join('')
+    // caroudleNavContainer.innerHTML = carouselNavItems.join('')
 
-    const sliderNavItem = document.querySelectorAll('.slider-nav-item')
-    sliderNavItem.forEach((el) => {
-        el.addEventListener('click', (e) => {
-            currentSlide = e.target.attributes.href.value
-        })
-    })
-    const slidePrevBtn = document.getElementById('sliderPrev')  
-    slidePrevBtn.addEventListener('click', () => {
-        if(currentSlide == '#slide-0'){
-            return
-        }
-        // if(currentSlide == '#slide-1'){
-        //     document.getElementById(0).classList.remove('active')
-        // }
-        let slide  = currentSlide.split('-')[1]
-        currentSlide = `#slide-${parseInt(slide) - 1}`
-        document.getElementById(currentSlide.split('-')[1]).classList.remove('active')
-        slidePrevBtn.href = currentSlide
-    })
+    // const sliderNavItem = document.querySelectorAll('.slider-nav-item')
+    // sliderNavItem.forEach((el) => {
+    //     el.addEventListener('click', (e) => {
+    //         currentSlide = e.target.attributes.href.value
+    //     })
+    // })
+    // const slidePrevBtn = document.getElementById('sliderPrev')  
+    // slidePrevBtn.addEventListener('click', () => {
+    //     if(currentSlide == '#slide-0'){
+    //         return
+    //     }
+    //     let slide  = currentSlide.split('-')[1]
+    //     currentSlide = `#slide-${parseInt(slide) - 1}`
+    //     document.getElementById(currentSlide.split('-')[1]).classList.remove('active')
+    //     slidePrevBtn.href = currentSlide
+    // })
 
-    const slideNextBtn = document.getElementById('sliderNext')  
-    slideNextBtn.addEventListener('click', () => {
-        if(currentSlide == `#slide-${arr.length - 1}`){
-            return
-        }
-        let slide  = currentSlide.split('-')[1]
-        currentSlide = `#slide-${parseInt(slide) + 1}`
-        document.getElementById(slide).classList.add('active')
-        slideNextBtn.href = currentSlide
-    })
+    // const slideNextBtn = document.getElementById('sliderNext')  
+    // slideNextBtn.addEventListener('click', () => {
+    //     if(currentSlide == `#slide-${arr.length - 1}`){
+    //         return
+    //     }
+    //     let slide  = currentSlide.split('-')[1]
+    //     currentSlide = `#slide-${parseInt(slide) + 1}`
+    //     document.getElementById(slide).classList.add('active')
+    //     slideNextBtn.href = currentSlide
+    // })
 
 });
 
@@ -365,7 +362,7 @@ function changeSide3(e) {
     }
 }
 // set initial side
-changeSide();
+// changeSide();
 
 // cube.addEventListener( 'mouseenter', () => {
 //     changeSide('enter') 
@@ -373,9 +370,9 @@ changeSide();
 // cube.addEventListener( 'mouseleave', () => {
 //     changeSide('leave') 
 // });
-cube.addEventListener('click', () => {
-    changeSide('click')
-})
+// cube.addEventListener('click', () => {
+//     changeSide('click')
+// })
 
 // cube2.addEventListener( 'mouseenter', () => {
 //     changeSide2('enter') }
@@ -383,11 +380,11 @@ cube.addEventListener('click', () => {
 // cube2.addEventListener( 'mouseleave', () => {
 //     changeSide2('leave') 
 // });
-cube2.addEventListener('click', () => {
-    changeSide2('click')
-})
+// cube2.addEventListener('click', () => {
+//     changeSide2('click')
+// })
 
-changeSide2()
+// changeSide2()
 
 // cube3.addEventListener( 'mouseenter', () => {
 //     changeSide3('enter') }
@@ -396,11 +393,11 @@ changeSide2()
 //     changeSide3('leave') 
 // }
 // );
-cube3.addEventListener('click', () => {
-    changeSide3('click')
-})
+// cube3.addEventListener('click', () => {
+//     changeSide3('click')
+// })
 
-changeSide3()
+// changeSide3()
 
 
 
