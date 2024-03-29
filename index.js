4324fec536f10ae61568e14d10322cd4809a0bfc
AkashBehara <behara.akash68@gmail.com>
2024-03-29 16:45:52 +0530

    horizontal scroll UI

--- index.js
+++ index.js
@@ -154,15 +154,16 @@ document.addEventListener('DOMContentLoaded', () => {
     xPercent: -100 * (sections.length - 1),
     ease: "none",
     scrollTrigger: {
-        trigger: ".container_gsap",
+        trigger: ".Main__container_gsap",
         start: "top 10%",
+        // end: "bottom 10%",
         pin: true,
         scrub: 1,
         snap: 1 / (sections.length - 1),
         // markers: true,
         // pinSpacing: false,
         // base vertical scrolling on how wide the container is so it feels more natural.
-        // end: "+=3500",
+        end: "+=3500",
     }
     });
 
